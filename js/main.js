e1aae752c0dbc12c5a0d8b6fb2dba561637ea0b4
HABAB0 <tolya.shaprove@mail.ru>
2026-02-04 14:43:34 +0700

    add: task component

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+Vue.component('table', {
+    props: {},
+    template: `
+ `,
+    data() {
+        return {}
+    },
+    methods: {},
+    computed: {},
+    mounted() {}
+})
+
+Vue.component('task', {
+    props: {},
+    template: `
+    <div v-for="paragraph in paragraphs"class="paragraphs">
+        <div class="paragraphs__item" >
+            {{ paragraph.description }}
+            <label>
+                <input type="checkbox" v-model="paragraph.checked">
+            </label>
+        </div>
+    </div>
+ `,
+    data() {
+        return {}
+    },
+    methods: {},
+    computed: {},
+    mounted() {}
+})
+
 let app = new Vue({
     el: '#app',
     data: {
