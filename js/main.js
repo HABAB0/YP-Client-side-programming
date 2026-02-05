de8552e47defa3ecd812937c842ecf55ce6f831c
HABAB0 <tolya.shaprove@mail.ru>
2026-02-05 21:40:53 +0700

    add: columns

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-Vue.component('table', {
-    props: {},
+Vue.component('task', {
+    props: {
+
+    },
     template: `
     <div>
-        <div v-for="task in tasks" class="paragraphs">
-            <div class="paragraphs__item" >
+         <div v-for="(paragraph, index) in paragraphs" class="paragraphs" :key="index">
+            <label class="paragraphs__item" >
+                {{ paragraph.name }}
+                <input type="checkbox" v-model="paragraph.checked">
+            </label>
+        </div>
+    </div>
+    
+ `,
+    data() {
+        return {
+            paragraphs: [
+                { name: 'это', checked: false },
+                { name: 'то', checked: false },
+                { name: 'четыре', checked: false }
+            ]
+        }
+    },
+    methods: {},
+    computed: {},
+    mounted() {}
+})
+
+Vue.component('column', {
+    props: {},
+    template: `
+    <div class="tasks">
+        <div v-for="task in tasks" class="tasks__container">
+            <div class="tasks__item" >
                 {{ task }}
                <task></task>
             </div>
@@ -13,32 +42,6 @@ Vue.component('table', {
     data() {
         return {
             tasks: ['Делать', 'сделать']
-        }
-    },
-    methods: {},
-    computed: {},
-    mounted() {}
-})
-
-Vue.component('task', {
-    props: {},
-    template: `
-    <div>
-        <div v-for="paragraph in paragraphs" class="paragraphs">
-            <div class="paragraphs__item" >
-                {{ paragraph }}
-                <label>
-                    <input type="checkbox" v-model="paragraph.checked">
-                </label>
-            </div>
-        </div>
-    </div>
-    
- `,
-    data() {
-        return {
-            paragraphs: ['это', 'то', 'чатыре'],
-
         }
     },
     methods: {},
