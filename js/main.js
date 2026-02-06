48770ed3b99f5a300658f05e7975f210cfabc370
HABAB0 <tolya.shaprove@mail.ru>
2026-02-06 15:38:29 +0700

    refactor components

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-Vue.component('table', {
-    props: {},
+Vue.component('task', {
+    props: {
+        tasks: Array,
+    },
     template: `
     <div>
-        <div v-for="task in tasks" class="paragraphs">
-            <div class="paragraphs__item" >
-                {{ task }}
-               <task></task>
-            </div>
+        <div v-for="task in tasks">
+            {{ task.name }}
+            <label>
+                <input type="checkbox" v-model="task.checked">
+            </label>
         </div>
-    </div>
+    </div>   
  `,
     data() {
         return {
-            tasks: ['Делать', 'сделать']
         }
     },
     methods: {},
@@ -20,25 +21,30 @@ Vue.component('table', {
     mounted() {}
 })
 
-Vue.component('task', {
-    props: {},
+Vue.component('column', {
+    props: {
+        cards: Array
+    },
     template: `
-    <div>
-        <div v-for="paragraph in paragraphs" class="paragraphs">
-            <div class="paragraphs__item" >
-                {{ paragraph }}
-                <label>
-                    <input type="checkbox" v-model="paragraph.checked">
-                </label>
+    <div class="column__container" >
+        <div v-for="column in columns" class="column__item">
+            {{ column.title }}
+            <div v-for="card in cards" class="card__item">
+                <div v-if="column.id == card.table">
+                    {{ card.title }}
+                    <task :tasks="card.tasks" ></task>
+                </div>
             </div>
         </div>
-    </div>
-    
+    </div>   
  `,
     data() {
         return {
-            paragraphs: ['это', 'то', 'чатыре'],
-
+            columns: [
+                {title: 'Первый'},
+                {title: 'Второй'},
+                {title: 'Третий'},
+            ]
         }
     },
     methods: {},
@@ -49,5 +55,16 @@ Vue.component('task', {
 let app = new Vue({
     el: '#app',
     data: {
+        cards: [
+            {title: 'Сейчас', table: '1',
+                tasks: [
+                    {name: '1', checked: false},
+                    {name: '2', checked: false}
+                ]},
+            {title: 'Завтра' , table: '0',
+                tasks: [
+                    {name: 'это', checked: false},
+                ]},
+        ]
     },
 })
